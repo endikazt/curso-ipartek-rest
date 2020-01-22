2a526ae75d519f4d5acd4c752fb7588ab55fa00d
Endika Zuazo <endikazt@hotmail.com>
2020-01-22 10:16:33 +0100

    Hechos los metodos GET, POST, PUT y DELETE

--- supermercado-rest/src/main/java/com/ipartek/formacion/supermercado/controller/ProductoRestController.java
+++ supermercado-rest/src/main/java/com/ipartek/formacion/supermercado/controller/ProductoRestController.java
@@ -15,8 +15,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
 import com.ipartek.formacion.supermercado.modelo.dao.ProductoDAO;
 import com.ipartek.formacion.supermercado.modelo.pojo.Producto;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 
 
 /**
@@ -50,6 +53,9 @@ public class ProductoRestController extends HttpServlet {
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		// prepara la response		
+		response.setContentType("application/json"); 
+		response.setCharacterEncoding("utf-8");
 		
 		super.service(request, response);   // llama a doGEt, doPost, doPut, doDelete
 		
@@ -63,24 +69,55 @@ public class ProductoRestController extends HttpServlet {
 		LOG.trace("peticion GET");
 		
 		String pathInfo = request.getPathInfo();
+		PrintWriter out = response.getWriter();
+		String jsonResponseBody = "";
 		
 		LOG.debug("mirar pathInfo:" + pathInfo + " para saber si es listado o detalle" );
 		
-		//obtener productos de la BD
-		ArrayList<Producto> lista = (ArrayList<Producto>) productoDao.getAll();
-		
-		// prepara la response		
-		response.setContentType("application/json"); 
-		response.setCharacterEncoding("utf-8");
-		
-		// response body
-		PrintWriter out = response.getWriter();		               // out se encarga de poder escribir datos en el body
-		String jsonResponseBody = new Gson().toJson(lista);		   // conversion de Java a Json
-		out.print(jsonResponseBody.toString()); 	
-		out.flush();                                               // termina de escribir datos en response body
-		
-		// response status code
-		response.setStatus( HttpServletResponse.SC_OK );
+		if("/".equals(pathInfo)) {
+			
+			//obtener productos de la BD
+			ArrayList<Producto> lista = (ArrayList<Producto>) productoDao.getAll();
+			
+			response.setStatus( HttpServletResponse.SC_OK);
+			
+			jsonResponseBody = new Gson().toJson(lista);	 // conversion de Java a Json	
+			out.print(jsonResponseBody.toString()); 		 // out se encarga de poder escribir datos en el body
+			out.flush();  								     // termina de escribir datos en response body	
+			
+		} else {
+				
+			try {
+				
+				int pathInfoData = Integer.parseInt(pathInfo.substring(1));
+				
+				Producto producto = productoDao.getById(pathInfoData);
+				
+				if(producto == null) {
+					
+					response.setStatus( HttpServletResponse.SC_ACCEPTED);
+					out.print("No existe ningun producto con ese ID :(");
+					
+				} else {
+					
+					response.setStatus( HttpServletResponse.SC_OK);
+					jsonResponseBody = new Gson().toJson(producto);	
+					out.print(jsonResponseBody.toString());
+					out.flush(); 	
+					
+				}
+				
+			} catch (Exception e) {
+				LOG.info("El parametro pasado no es un numero. Error -> " + e);
+				
+				// response status code
+				response.setStatus( HttpServletResponse.SC_BAD_REQUEST);
+				out.print("Tienes que enviar el tipo de datos correctos. Ejemplo supermecado-rest/producto/1");
+				
+			}
+			
+			
+		}
 		
 	}
 
@@ -91,28 +128,168 @@ public class ProductoRestController extends HttpServlet {
 		
 		LOG.debug("POST crear recurso");
 		
+		PrintWriter out = response.getWriter();
+		
 		// convertir json del request body a Objeto
 		BufferedReader reader = request.getReader();               
 		Gson gson = new Gson();
-		Producto producto = gson.fromJson(reader, Producto.class);
 		
-		LOG.debug(" Json convertido a Objeto: " + producto);
-		
-		response.setStatus( HttpServletResponse.SC_NOT_IMPLEMENTED );
+		Producto producto;
+		try {
+			producto = gson.fromJson(reader, Producto.class);
+			
+			try {
+				productoDao.create(producto);
+				
+				response.setStatus( HttpServletResponse.SC_CREATED);
+				out.print("Producto creado correctamente :)");
+				
+			} catch (MySQLIntegrityConstraintViolationException esql) {
+				
+				LOG.error("No se ha podido insertar el registro en la base de datos. Error -> " + esql);
+				
+				response.setStatus( HttpServletResponse.SC_CONFLICT);
+				out.print("No se ha podido crear el producto porque el ID o el Nombre ya existen en la base de datos.");
+				
+				
+			} catch (Exception e) {
+				LOG.error(e);
+				
+				response.setStatus( HttpServletResponse.SC_CONFLICT);
+				out.print("No se ha podido crear el producto por algun error de la base de datos.");
+			}
+			
+			
+		} catch (JsonSyntaxException e1) {
+			LOG.error(e1);
+
+			response.setStatus( HttpServletResponse.SC_CONFLICT);
+			out.print("El formato de los datos es incorrecto.");
+			
+		} catch (JsonIOException e1) {
+			LOG.error(e1);
+			
+			response.setStatus( HttpServletResponse.SC_CONFLICT);
+			out.print("Tienes que enviar el tipo de datos correctos.");
+			
+		}
+	
 	}
 
 	/**
 	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
 	 */
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setStatus( HttpServletResponse.SC_NOT_IMPLEMENTED );
+		LOG.debug("PUT modificar recurso");
+		
+		String pathInfo = request.getPathInfo();
+		PrintWriter out = response.getWriter();
+		String jsonResponseBody = "";
+		BufferedReader reader = request.getReader();               
+		Gson gson = new Gson();
+		
+		if("/".equals(pathInfo)) {
+			
+			response.setStatus( HttpServletResponse.SC_BAD_REQUEST);
+			out.print("Tienes que indicar el producto que quieres modificar. Ejemplo supermecado-rest/producto/1");
+			
+		} else {
+				
+			try {
+				
+				int pathInfoData = Integer.parseInt(pathInfo.substring(1));
+				
+				Producto producto = productoDao.getById(pathInfoData);
+				
+				if(producto == null) {
+					
+					response.setStatus( HttpServletResponse.SC_ACCEPTED);
+					out.print("No existe ningun producto con ese ID :(");
+					
+				} else {
+					
+					try {
+						
+						producto = gson.fromJson(reader, Producto.class);
+						
+						response.setStatus( HttpServletResponse.SC_CREATED);
+						
+						jsonResponseBody = new Gson().toJson(producto);	
+						out.print("Producto modificado correctamente.");
+						out.print(jsonResponseBody.toString());
+						out.flush(); 	
+					
+						
+					} catch (Exception e) {
+						LOG.error(e);
+
+						response.setStatus( HttpServletResponse.SC_CONFLICT);
+						out.print("El formato de los datos es incorrecto.");
+						
+					}
+				}
+						
+				
+			} catch (Exception e) {
+				LOG.info("El parametro pasado no es un numero. Error -> " + e);
+				
+				// response status code
+				response.setStatus( HttpServletResponse.SC_BAD_REQUEST);
+				out.print("Tienes que enviar el tipo de datos correctos. Ejemplo supermecado-rest/producto/1");
+				
+			}	
+			
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
 	 */
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setStatus( HttpServletResponse.SC_NOT_IMPLEMENTED );
+		LOG.debug("DELETE eliminar recurso");
+		
+		String pathInfo = request.getPathInfo();
+		PrintWriter out = response.getWriter();
+		String jsonResponseBody = "";
+		
+		LOG.debug("mirar pathInfo:" + pathInfo + " para saber si es listado o detalle" );
+		
+		if("/".equals(pathInfo)) {
+			
+			response.setStatus( HttpServletResponse.SC_BAD_REQUEST);
+			out.print("Tienes que indicar el producto que quieres eliminar. Ejemplo supermecado-rest/producto/1");			     // termina de escribir datos en response body	
+			
+		} else {
+				
+			try {
+				
+				int pathInfoData = Integer.parseInt(pathInfo.substring(1));
+				
+				Producto producto = productoDao.getById(pathInfoData);
+				
+				if(producto == null) {
+					
+					response.setStatus( HttpServletResponse.SC_ACCEPTED);
+					out.print("No existe ningun producto con ese ID :(");
+					
+				} else {
+					
+					productoDao.delete(pathInfoData);
+					
+					response.setStatus( HttpServletResponse.SC_OK);
+					
+				}
+				
+			} catch (Exception e) {
+				LOG.info("El parametro pasado no es un numero. Error -> " + e);
+				
+				// response status code
+				response.setStatus( HttpServletResponse.SC_BAD_REQUEST);
+				out.print("Tienes que enviar el tipo de datos correctos. Ejemplo supermecado-rest/producto/1");
+				
+			}
+			
+		}
 	}
 
 }
